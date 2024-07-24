6290e31c96121a893d8360b5645898595f65154b
Adititanwar <130069667+Adititanwar@users.noreply.github.com>
2024-07-24 08:55:11 +0530

    Time: 4 ms (31.12%) | Memory: 10.1 MB (10.64%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       int zeroes =0;
-       int ones =0;
-       int n = nums.size();
-       for(int num : nums){
-        if( num ==0) zeroes ++;
-        else if ( num == 1 )ones ++;
-       }
-       for( int i = 0; i < zeroes ; ++i){
-        nums[i]=0;
-       }
+        int zeroes =0;
+        int ones =0;
+        int n = nums.size();
+        for( int num :nums){
+            if (num ==0 )zeroes ++;
+           else if(num == 1)ones++;
 
-       for (int i = zeroes ;i < zeroes + ones ;++i){
-        nums[i]=1;
-    
-       }
-       for(int i = zeroes + ones ; i < n ;++i){
-        nums[i] =2;
-       }
+        }
+        for( int i =0; i < zeroes ; ++i){
+            nums[i]= 0;
 
+        }
+        for( int i = zeroes ; i < zeroes + ones ;++i){
+            nums[i]=1;
+        }
+        for(int i =zeroes + ones ; i < n ; ++i){
+            nums[i]=2;        }
     }
+
 };
